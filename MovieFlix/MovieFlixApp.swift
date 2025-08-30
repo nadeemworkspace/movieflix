4466426efb1e8ad128d738c7abfaec9de3a22266
Muhammed Nadeem <muhammednadeem989@gmail.com>
2025-08-30 22:35:12 +0530

    initial commit

---
+++ MovieFlix/MovieFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFlixApp.swift
+//  MovieFlix
+//
+//  Created by Muhammed Nadeem on 30/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

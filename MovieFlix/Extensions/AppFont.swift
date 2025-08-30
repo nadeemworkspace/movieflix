05752f35929a3012ea3ca2062f8c436bb41b2774
Muhammed Nadeem <muhammednadeem989@gmail.com>
2025-08-30 23:06:46 +0530

    custom fonts added

---
+++ MovieFlix/Extensions/AppFont.swift
@@ -0,0 +1,23 @@
+//
+//  AppFont.swift
+//  MovieFlix
+//
+//  Created by Muhammed Nadeem on 30/08/25.
+//
+
+import SwiftUI
+
+enum AppFont: String {
+    case black      = "Poppins-Black"
+    case bold       = "Poppins-Bold"
+    case extraLight = "Poppins-ExtraLight"
+    case medium     = "Poppins-Medium"
+    case semiBold   = "Poppins-SemiBold"
+    case thin       = "Poppins-Thin"
+}
+
+extension Font {
+    static func poppins(_ type: AppFont, size: CGFloat) -> Font {
+        return .custom(type.rawValue, size: size)
+    }
+}

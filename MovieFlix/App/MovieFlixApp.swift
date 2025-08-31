fdd7f0cf979fddfe18409ecbf4d05367280242e9
Muhammed Nadeem <muhammednadeem989@gmail.com>
2025-08-31 17:49:39 +0530

    firebase added to the project

---
+++ MovieFlix/App/MovieFlixApp.swift
@@ -0,0 +1,32 @@
+//
+//  MovieFlixApp.swift
+//  MovieFlix
+//
+//  Created by Muhammed Nadeem on 30/08/25.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+    
+}
+
+@main
+struct MovieFlixApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                LoginView()
+            }
+        }
+    }
+}
---
+++ MovieFlix/Login/View/LoginView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginView.swift
+//  MovieFlix
+//
+//  Created by Muhammed Nadeem on 30/08/25.
+//  Copyright © 2025 genysis.inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
